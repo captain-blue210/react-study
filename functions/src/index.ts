e1accb506dfb917eeddf7357162989b2b537e5f9
captain-blue210 <captain.blue210@gmail.com>
2020-11-01 21:47:37 +0900

    add Cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,52 @@
-// import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import puppeteer from 'puppeteer';
+import { feedCalendar } from './crewlers/kodansha-calendar';
+import { saveFeedMemo } from './firestore-admin/feed-memo';
+import { collectionName } from './services/react-study/contants';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const PUPPETEER_OPTIONS = {
+  args: [
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+    '--single-process',
+  ],
+  headless: true,
+};
+
+admin.initializeApp();
+
+export const fetchCalendar = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '2GB',
+  })
+  .pubsub.schedule('0 2 1,10,20 * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async () => {
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
+    const page = await browser.newPage();
+    const db = admin.firestore();
+
+    const memos = await feedCalendar(page);
+    const fetchCount = await saveFeedMemo(db, memos, 'kodansha');
+
+    await browser.close();
+    console.log(`Fetched Kodansha calendar. Wrote ${fetchCount} memos.`);
+  });
+
+export const publishers = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.publishers)
+      .get();
+    const data = snap.docs.map((doc) => doc.data());
+    res.send({ data });
+  });
--- functions/src/services/react-study/contants.ts
+++ functions/src/services/react-study/contants.ts
@@ -1,8 +1,8 @@
 export const collectionName = {
-    authors: 'authors',
-    books: 'books',
-    users: 'users',
-    publishers: 'publishers',
-    docCounters: 'docCounters',
-    feedMemos: 'feedMemos',
-  } as const;
+  authors: 'authors',
+  books: 'books',
+  users: 'users',
+  publishers: 'publishers',
+  docCounters: 'docCounters',
+  feedMemos: 'feedMemos',
+} as const;
